52b88b84b95713cd5dd7f9678009c3394d5cf046
Vladimir Pomsztein <vladimir.pomsztein@mercadolibre.com>
2020-03-02 14:20:35 -0300

    Initial project setup

---
+++ Swind/Swind.h
@@ -0,0 +1,19 @@
+//
+//  Swind.h
+//  Swind
+//
+//  Created by Vladimir Pomsztein on 3/2/20.
+//  Copyright © 2020 Vladimir Pomsztein. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swind.
+FOUNDATION_EXPORT double SwindVersionNumber;
+
+//! Project version string for Swind.
+FOUNDATION_EXPORT const unsigned char SwindVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swind/PublicHeader.h>
+
+
