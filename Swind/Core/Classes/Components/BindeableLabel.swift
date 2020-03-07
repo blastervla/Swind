94540cbfe285c228bf3f91d636c2f7500e04dd5d
blastervla <blastervla@gmail.com>
2020-03-07 19:25:16 -0300

    v0.3.0 Add bindeable labels and cardviews

---
+++ Swind/Core/Classes/Components/BindeableLabel.swift
@@ -0,0 +1,57 @@
+//
+//  BindeableLabel.swift
+//  Swind
+//
+//  Created by Vladimir Pomsztein on 07/03/2020.
+//
+
+import UIKit
+
+public class BindeableLabel: UILabel {
+
+    public var onTap: (() -> Void)? = nil
+    public var onLongTap: (() -> Void)? = nil
+    var tapBindeeSelector: Selector?
+    var tapBindee: NSObject?
+    var longTapBindeeSelector: Selector?
+    var longTapBindee: NSObject?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupGestureRecognizers()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupGestureRecognizers()
+    }
+    
+    private func setupGestureRecognizers() {
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(didTap))
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(didLongTap))
+        self.isUserInteractionEnabled = true
+        self.addGestureRecognizer(singleTap)
+        self.addGestureRecognizer(longPress)
+    }
+    
+    public func bind(_ bindee: NSObject, _ bindeeSelector: Selector) {
+        self.tapBindee = bindee
+        self.tapBindeeSelector = bindeeSelector
+    }
+    
+    public func bindForLongTap(_ bindee: NSObject, _ bindeeSelector: Selector) {
+        self.longTapBindee = bindee
+        self.longTapBindeeSelector = bindeeSelector
+    }
+    
+    @objc func didTap(sender: UIButton) {
+        self.onTap?()
+        self.tapBindee?.perform(self.tapBindeeSelector)
+    }
+    
+    @objc func didLongTap(sender: UIButton) {
+        self.onLongTap?()
+        self.longTapBindee?.perform(self.longTapBindeeSelector)
+    }
+
+}
